649dbf1a53c8e0c82399fbadca99f38813225986
aminnepali <57822354+AMRhunter31@users.noreply.github.com>
2023-02-13 13:06:37 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
-const localVideo = document.getElementById('local-video');
-const remoteVideo = document.getElementById('remote-video');
+const chatBox = document.getElementById('chat-box');
+const chatForm = document.getElementById('chat-form');
+const chatInput = document.getElementById('chat-input');
 
-// Request access to the camera and microphone
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then(stream => {
-    // Display the video stream from the camera in the local video element
-    localVideo.srcObject = stream;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// Listen for a new message
+chatForm.addEventListener('submit', event => {
+  event.preventDefault();
 
-// Connect to a peer using WebRTC
-const peerConnection = new RTCPeerConnection();
+  // Get the value of the chat input
+  const message = chatInput.value;
 
-// Listen for a new candidate coming from the remote peer
-peerConnection.addEventListener('icecandidate', event => {
-  if (event.candidate) {
-    // Send the candidate to the remote peer
-  }
+  // Append the message to the chat box
+  const messageElement = document.createElement('div');
+  messageElement.className = 'message';
+  messageElement.textContent = message;
+  chatBox.appendChild(messageElement);
+
+  // Clear the chat input
+  chatInput.value = '';
 });
-
-// Listen for a new stream coming from the remote peer
-peerConnection.addEventListener('addstream', event => {
-  // Display the video stream from the remote peer in the remote video element
-  remoteVideo.srcObject = event.stream;
-});
-
-// Add the local stream to the peer connection
-peerConnection.addStream(localVideo.srcObject);
-
-// Initiate the connection
-peerConnection.createOffer()
-  .then(offer => {
-    // Set the local description using the offer
-    return peerConnection.setLocalDescription(offer);
-  })
-  .then(() => {
-    // Send the offer to the remote peer
-  })
-  .catch(error => {
-    console.error(error);
-  });
