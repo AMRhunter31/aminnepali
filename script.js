cb668757d07ab1e3ec990dbb7209a3dea9164ae3
aminnepali <57822354+AMRhunter31@users.noreply.github.com>
2023-04-02 19:43:00 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,34 @@
-fetch('./files/')
+fetch("./files/")
   .then(response => response.text())
   .then(data => {
-    const files = data.split('\n').filter(file => file.trim() !== '');
-    const list = document.getElementById('file-list');
+    const files = data.split("\n").filter(file => file.trim() !== "");
+    const list = document.getElementById("file-list");
 
     files.forEach(file => {
-      const div = document.createElement('div');
-      div.classList.add('file');
-
-      const link = document.createElement('a');
+      const li = document.createElement("li");
+      const link = document.createElement("a");
       link.href = `./files/${file}`;
-      link.setAttribute('download', '');
-      
-      const icon = document.createElement('span');
-      icon.classList.add('file-icon');
-      icon.textContent = '📎';
-
-      const name = document.createElement('span');
-      name.classList.add('file-name');
+      const icon = document.createElement("span");
+      icon.classList.add("file-icon");
+      icon.textContent = "📂";
+      const name = document.createElement("span");
       name.textContent = file;
-
-      const button = document.createElement('button');
-      button.textContent = 'Download';
-
+      const size = document.createElement("span");
+      size.classList.add("file-size");
+      fetch(`./files/${file}`)
+        .then(response => response.headers.get('content-length'))
+        .then(sizeInBytes => {
+          size.textContent = `${(sizeInBytes/1024).toFixed(2)} KB`;
+        })
+        .catch(error => {
+          console.error(error);
+          size.textContent = "unknown";
+        });
       link.appendChild(icon);
       link.appendChild(name);
-      div.appendChild(link);
-      div.appendChild(button);
-      list.appendChild(div);
-
-      button.addEventListener('click', (event) => {
-        event.stopPropagation();
-        button.disabled = true;
-        fetch(`./files/${file}`)
-          .then(response => response.blob())
-          .then(blob => {
-            const objectUrl = URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = objectUrl;
-            a.download = file;
-            a.click();
-            URL.revokeObjectURL(objectUrl);
-            button.disabled = false;
-          })
-          .catch(error => {
-            console.error(error);
-            button.disabled = false;
-          });
-      });
+      li.appendChild(link);
+      li.appendChild(size);
+      list.appendChild(li);
     });
   })
   .catch(error => {
