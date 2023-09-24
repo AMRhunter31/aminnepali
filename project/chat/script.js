f3e73b14933308fc51afadad5e10b6f27c52ea68
aminnepali <57822354+AMRhunter31@users.noreply.github.com>
2023-09-24 21:32:42 +0545

    dfdf

--- project/chat/script.js
+++ project/chat/script.js
@@ -1,48 +1,20 @@
-const pusher = new Pusher({
-    appId: '1676291',
-    key: '1729bbaddaf906e43c7f',
-    secret: '348a5b60c490de2856cc',
-    cluster: 'ap2',
-    encrypted: true
-});
-
-const channel = pusher.subscribe('ameen shareonline'); // Use the correct channel name
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-const userColor = getRandomColor();
-
-channel.bind('message', function(data) {
-    const chatBox = document.getElementById('chat-box');
-    const messageClass = data.user === '1' ? 'user-1-message' : 'user-2-message';
-    chatBox.innerHTML += `<p class="${messageClass}" style="color: ${userColor}">User ${data.user}: ${data.text}</p>`;
-    chatBox.scrollTop = chatBox.scrollHeight;
-});
+const chatBox = document.getElementById('chat-box');
+const messageInput = document.getElementById('message-input');
 
 function sendMessage() {
-    const messageInput = document.getElementById('message-input');
     const messageText = messageInput.value.trim();
-    
+
     if (messageText !== '') {
-        const message = {
-            user: '1', // Replace with the user ID (1 for User 1, 2 for User 2, and so on)
-            text: messageText,
-        };
+        // Create a new message element and add it to the chat box
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.textContent = 'You: ' + messageText;
+        chatBox.appendChild(messageElement);
 
-        channel.trigger('message', message);
+        // Clear the input field
+        messageInput.value = '';
 
-        const chatBox = document.getElementById('chat-box');
-        const messageClass = message.user === '1' ? 'user-1-message' : 'user-2-message';
-        chatBox.innerHTML += `<p class="${messageClass}" style="color: ${userColor}">You: ${messageText}</p>`;
+        // Scroll to the bottom to show the latest message
         chatBox.scrollTop = chatBox.scrollHeight;
-
-        messageInput.value = ''; // Clear the input field
     }
 }
